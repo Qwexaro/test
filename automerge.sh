7947578da69fc719886e8f8ee02fd62fdb75b01e
Qwexaro <Sergikstar25.gold@mail.ru>
2025-05-17 13:25:44 +0300

    Main

--- automerge.sh
+++ automerge.sh
@@ -1,3 +1,15 @@
+echo "your current branches: "
+
+git branch
+
+read -p "enter name of branch for switched: " switch_branch
+
+git checkout "$switch_branch"
+
+echo "your current branch: "
+
+git branch
+
 read -p "decide branch for rewrite code in current branch: " branch
 
 git merge "$branch"
